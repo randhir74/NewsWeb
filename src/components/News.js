43d9ad0b099b9bbff476e4f9804dd37e0db11f63
randhir74 <randhirkumar1062000@gmail.com>
2023-06-01 10:06:20 +0530

    I have developed my News API for the first time, It is working fine with all of my current functionality without any error. There are many things that I will add in future

---
+++ src/components/News.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'//Proptypes in class based components
+import config from './config.json';
+
+const apiKey = config.apiKey;
+export class News extends Component {
+    static defaultProps = {
+        country: 'in',
+        pageSize: 8, 
+        category: 'general',
+        apiKey: {apiKey}
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category:PropTypes.string
+    }
+    capitalizeFirstLetter = (string)=> {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    constructor(props) {
+        super(props);
+        console.log(apiKey)
+        this.state = {
+            articles: [], 
+            loading: false,
+            page: 1
+        }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
+    }
+
+    async updateNews(){
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${apiKey}&
+        page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles, 
+            totalResults: parsedData.totalResults,
+            loading: false
+        })
+    }
+
+    async componentDidMount(){
+        this.updateNews();
+    }
+
+    handlePrevClick = async ()=>{
+        this.setState({page: this.state.page-1})
+        this.updateNews();
+    }
+
+    handleNextClick = async ()=>{
+        console.log("Next")
+        this.setState({page: this.state.page+1})
+        this.updateNews();
+    } 
+
+    render() {
+        return (
+            <div className='container my-3'>
+                <h1 className='text-center' style={{margin: '35px 0px'}}>NewsMonkey - Top Headlines on {this.capitalizeFirstLetter(this.props.category)}</h1>
+                {this.state.loading && <Spinner/>}
+                <div className="row">
+                    {!this.state.loading && this.state.articles.map((element) => {
+                            return (<div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+                            </div>)
+                        }
+                        )
+                    }
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr;   Previous</button>
+                    <button disabled={this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next  &rarr;</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default News
